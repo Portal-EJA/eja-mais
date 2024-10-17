dd852141f019b3f1f1fffa2de6c1787ebff713cf
vinicius010neto <74117559+vinicius010neto@users.noreply.github.com>
2024-10-17 11:26:27 -0300

    Criação lógica do context

--- src/components/header/navbar-links.tsx
+++ src/components/header/navbar-links.tsx
@@ -1,6 +1,9 @@
 import { sections } from "../../db/data"
+import { useAppContext } from "../../context/AppContext"
 
 export function NavBarLinks() {
+  const { setId } = useAppContext();
+
   return (
     <ul
       className="flex flex-col md:flex-row items-center justify-between gap-4 w-full"
@@ -12,13 +15,15 @@ export function NavBarLinks() {
           key={`${i}-${section.name}`}
           id={section.id}
         >
-          {/* trocar pois nao vai ser um href e sim um botao que seta um contexto */}
-          <a
-            href={`#${section.id}`}
+          {/* trocar pois nao vai ser um href e sim um botao que seta um contexto 
+            Troquei:) 
+          */}
+          <button
+            onClick={() => setId(section.id)} 
             className="leading-relaxed font-medium text-md lg:text-base hover:font-semibold transition-all"
           >
             {section.name}
-          </a>
+          </button>
         </li>
       ))}
     </ul>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import { App } from "./app.tsx"
-import "./styles/index.css"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./app.tsx";
+import "./styles/index.css";
+import { AppProvider } from "./context/AppContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+  <AppProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </AppProvider>
+);
